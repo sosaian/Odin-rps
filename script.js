febe39d5a4947d8ca0db4ef201c3117ac3d3de8a
Sosa Ian <58917248+sosaian@users.noreply.github.com>
2023-05-06 04:22:14 -0300

    Add playRound function alongside code for testing it

--- script.js
+++ script.js
@@ -9,3 +9,41 @@ function getComputerChoice()
         else
                 return "Scissors";
 }
+
+function playRound(playerSelection, computerSelection)
+{
+    if (playerSelection === computerSelection)
+        return `Draw! both of you chose ${playerSelection}`;
+    else
+    {
+        switch (playerSelection)
+        {
+            case ("Rock"):
+            {
+                if (computerSelection === "Scissors")
+                    return `You Win! ${playerSelection} beats ${computerSelection}`;
+                else
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            }
+            case (Paper):
+            {
+                if (computerSelection === "Rock")
+                    return `You Win! ${playerSelection} beats ${computerSelection}`;
+                else
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            }       
+            default:
+            {   //playerSelection = "Scissors"
+                if (computerSelection === "Paper")
+                    return `You Win! ${playerSelection} beats ${computerSelection}`;
+                else
+                    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+            }
+        }   //Note: thanks to using return in each case, there's
+            //no need for break statements :)
+    }
+}
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
